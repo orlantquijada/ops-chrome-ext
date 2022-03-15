1097db0e7e6d12d09724eb74c9f5d154d7d66751
Orlan Quijada <orlantquijada@gmail.com>
2022-03-15 18:44:48 +0800

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,46 @@
-import { useState } from 'react';
+import { useState } from 'react'
+
+import { globalStyles } from '../stitches.config'
+import { Link } from 'react-router-dom'
+import * as Dialog from './components/Dialog'
 
 function App() {
-  const [count, setCount] = useState(0);
+  globalStyles()
+  const [count, setCount] = useState(0)
+
+  const handleStartExam = () => {}
+  const handleEndExam = () => {}
 
   return (
-    <div>
+    <div style={{ width: 320, height: 550 }}>
       <h1>{count}</h1>
       <div>
+        <nav
+          style={{
+            borderBottom: 'solid 1px',
+            paddingBottom: '1rem',
+          }}
+        >
+          <Link to="/invoices">Invoices</Link> |{' '}
+          <Link to="/expenses">Expenses</Link>
+        </nav>
+
         <button onClick={() => setCount((c) => c + 1)}>+</button>
         <button onClick={() => setCount((c) => c - 1)}>-</button>
+        <button onClick={handleStartExam}>start</button>
+        <button onClick={handleEndExam}>end</button>
+        <h2>asdasd</h2>
+        <Dialog.Root>
+          <Dialog.Trigger>Wow</Dialog.Trigger>
+          <Dialog.Content>
+            <Dialog.Title css={{ color: '$bloo-light-primary' }}>
+              title
+            </Dialog.Title>
+          </Dialog.Content>
+        </Dialog.Root>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import { MemoryRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
+import Expenses from './routes/expenses'
+import Invoices from './routes/invoices'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MemoryRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="expenses" element={<Expenses />} />
+        <Route path="invoices" element={<Invoices />} />
+      </Routes>
+    </MemoryRouter>
   </React.StrictMode>,
   document.getElementById('root')
 )
