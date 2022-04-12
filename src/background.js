9aa442d7c9ff0786d40daebb465e9d2bbc72009b
Orlan Quijada <orlantquijada@gmail.com>
2022-04-13 02:50:47 +0800

    feat: accessed site stuff + wip switch applications

--- src/background.js
+++ src/background.js
@@ -5,18 +5,21 @@ async function startExamHandler() {
   ])
 
   // listen for `alt-tabs`
-  window.addEventListener('blur', async () => {
-    fetch('http://127.0.0.1:8000/api/activities', {
-      body: JSON.stringify({
-        name: 'LOSE_WINDOW_FOCUS',
-        description: 'has opened another application.',
-        examId,
-        examineeId,
-        isSuspicious: true,
-      }),
-      headers: { 'Content-Type': 'application/json' },
-      method: 'POST',
-    })
+  window.addEventListener('blur', async (asd) => {
+    const { url, activeTabUrl } = await asd.target.chrome.storage.sync.get()
+
+    if (url === activeTabUrl)
+      fetch('http://127.0.0.1:8000/api/activities', {
+        body: JSON.stringify({
+          name: 'LOSE_WINDOW_FOCUS',
+          description: 'has opened another application.',
+          examId,
+          examineeId,
+          isSuspicious: true,
+        }),
+        headers: { 'Content-Type': 'application/json' },
+        method: 'POST',
+      })
   })
 
   // wip: confirm exit exam
@@ -34,27 +37,33 @@ async function handler(activeInfo) {
   const { examId, examineeId, url, startExam } = await chrome.storage.sync.get()
 
   const tab = await chrome.tabs.get(activeInfo.tabId)
+  await chrome.storage.sync.set({ activeTabUrl: tab.url })
 
-  if (startExam)
+  if (startExam) {
+    const returned = url === tab.url
+
     fetch('http://127.0.0.1:8000/api/activities', {
       body: JSON.stringify({
-        name: url === tab.url ? 'RETURNED' : 'SWITCHED_TAB',
-        description:
-          url === tab.url
-            ? `has returned to the exam tab. ${url} ${activeInfo.tabId}`
-            : `switched to another tab ${url} ${tab.url}`,
+        name: returned ? 'RETURNED' : 'SWITCHED_TAB',
+        description: returned
+          ? 'has returned to the exam tab.'
+          : 'switched to another tab.',
         examId,
         examineeId,
-        isSuspicious: true,
+        isSuspicious: !returned,
       }),
       headers: { 'Content-Type': 'application/json' },
       method: 'POST',
     })
+  }
 }
 
 chrome.tabs.onActivated.addListener(handler)
 chrome.tabs.onUpdated.addListener(async (_, changeInfo, tab) => {
-  // wip
+  if (!changeInfo.url) return
+
+  // search engine usage is tracked in `tabs.onUpdate` since the alternative, `history.onVisited`
+  // does not track usage in incognito
   const { examId, examineeId } = await chrome.storage.sync.get()
 
   const searchEngines = [
@@ -82,10 +91,7 @@ chrome.tabs.onUpdated.addListener(async (_, changeInfo, tab) => {
     return new URL(url).searchParams.get('q')
   }
 
-  if (
-    changeInfo.url &&
-    searchEngines.some((searchEngine) => tab.url.includes(searchEngine))
-  )
+  if (searchEngines.some((searchEngine) => tab.url.includes(searchEngine)))
     fetch('http://127.0.0.1:8000/api/activities', {
       body: JSON.stringify({
         name: 'USED_SEARCH_ENGINE',
@@ -99,6 +105,19 @@ chrome.tabs.onUpdated.addListener(async (_, changeInfo, tab) => {
       headers: { 'Content-Type': 'application/json' },
       method: 'POST',
     })
+  else if (!tab.url.startsWith('chrome')) {
+    fetch('http://127.0.0.1:8000/api/activities', {
+      body: JSON.stringify({
+        name: 'ACCESSED_SITE',
+        description: `has accessed ${tab.url}.`,
+        examId,
+        examineeId,
+        isSuspicious: true,
+      }),
+      headers: { 'Content-Type': 'application/json' },
+      method: 'POST',
+    })
+  }
 })
 
 chrome.storage.onChanged.addListener(async (changes) => {
