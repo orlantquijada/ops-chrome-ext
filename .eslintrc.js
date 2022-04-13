80dbb5a4ab94e881c2d603671363f532aa2f56d8
Orlan Quijada <orlantquijada@gmail.com>
2022-04-14 04:43:30 +0800

    chore: eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'prettier/prettier': 'error',
+  },
+  globals: {
+    chrome: 'readonly',
+  },
+}
