a48b44c7f0809d7648e42bc53f89a0a083fb0635
citReyJoshua <reyjoshua.macarat@cit.edu>
2022-04-28 14:36:12 +0800

    chore: instructions ui for enabling incognito access

--- src/routes/EnableIncognitoScreen.tsx
+++ src/routes/EnableIncognitoScreen.tsx
@@ -1,6 +1,7 @@
 import { styled } from '../../stitches.config'
 import Layout from '../components/Layout'
 import Logo from '../components/Logo'
+import Text from '../components/Text'
 
 export default function EnableIncognitoScreen() {
   return (
@@ -9,7 +10,35 @@ export default function EnableIncognitoScreen() {
         <Logo />
       </Header>
 
-      <Main>please enable incognito</Main>
+      <Main>
+        <Title>Welcome to OPS!</Title>
+        <Text
+          as="p"
+          fontSize="sm"
+          color="bloo-dark-primary"
+          css={{ paddingBlock: '1rem' }}
+        >
+          It seems like you have not authorized this extension to work in
+          incognito. Please follow the instructions below to allow incognito
+          access.
+        </Text>
+        <OL>
+          <LI>
+            On your web browser, navigate to the <b>Extensions</b> settings page{' '}
+            <u>chrome://extensions</u>.
+          </LI>
+          <LI>
+            Find the <b>OPS</b> browser extension.
+          </LI>
+          <LI>
+            Click on <b>Details</b>.
+          </LI>
+          <LI>
+            Find <b> Allow in Incognito </b> option and turn it on.
+          </LI>
+          <LI>Reopen the browser extension.</LI>
+        </OL>
+      </Main>
     </Layout>
   )
 }
@@ -22,4 +51,21 @@ const Header = styled('header', {
 
 const Main = styled('main', {
   mt: '$5',
+})
+
+const OL = styled('ol', {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+  alignItems: 'space-between',
+  gapy: '$1',
+  w: '100%',
+})
+const LI = styled('li', {
+  fontSize: '$3',
+  color: '$bloo-light-primary',
+})
+
+const Title = styled('h1', {
+  color: '$bloo-dark-20',
 })
